3d3b2fa503d12c23b1d2fbfcea09891969a8292b
Petr Štetiar <ynezz@true.cz>
2017-08-30 11:13:40 +0200

    build/local.mk: Add basic command set
    Signed-off-by: Petr Štetiar <ynezz@true.cz>

---
+++ build/local.mk
@@ -0,0 +1,16 @@
+v ?= 0
+DEBUG_BUILD ?= n
+APP ?= modbus
+USE_SWD ?= y
+MF_DFU ?= all program-dfu
+MF_OCD ?= all program-ocd
+MF_FLASH ?= $(MF_OCD)
+MF_DEBUG ?= PARTICLE_DEVELOP=1 PLATFORM=photon MODULAR=n
+MF_RELEASE ?= PLATFORM=photon DEBUG_BUILD=n USE_SWD=n USE_SWD=n
+MAKE_FLAGS ?= $(MF_DEBUG) v=$(v) APP=$(APP) USE_SWD=$(USE_SWD)
+
+modbus-flash:
+	make $(MF_FLASH) $(MAKE_FLAGS)
+
+modbus-test:
+	make clean all program-dfu TEST=wiring/no_fixture PLATFORM=photon
